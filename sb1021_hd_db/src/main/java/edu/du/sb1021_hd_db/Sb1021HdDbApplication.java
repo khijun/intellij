5fda31c4c49baa1e8e81411dab722684086b6ba4
khijun <khijun00@gmail.com>
2024-10-21 16:35:54 +0900

    1021

---
+++ sb1021_hd_db/src/main/java/edu/du/sb1021_hd_db/Sb1021HdDbApplication.java
@@ -0,0 +1,13 @@
+package edu.du.sb1021_hd_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb1021HdDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb1021HdDbApplication.class, args);
+    }
+
+}
---
+++ sb1021_hd_db/src/test/java/edu/du/sb1021_hd_db/Sb1021HdDbApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.du.sb1021_hd_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb1021HdDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
